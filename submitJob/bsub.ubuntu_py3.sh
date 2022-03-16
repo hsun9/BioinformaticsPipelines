8071374779f6ee3e840c5389e4158f8cee438390
Hua Sun <33794673+hsun9@users.noreply.github.com>
2022-03-15 20:25:04 -0500

    Update bsub.ubuntu_py3.sh

--- submitJob/bsub.ubuntu_py3.sh
+++ submitJob/bsub.ubuntu_py3.sh
@@ -14,6 +14,6 @@ DIR=`pwd`
 LOG=$DIR/logs
 mkdir -p $LOG
 
-bsub -G mackgrp -q general -M ${MEM}000000 -n ${THREADS} -R "select[mem>${MEM}000] span[hosts=1] rusage[mem=${MEM}000]" -oo ${LOG}/${NAME}.log -eo ${LOG}/${NAME}.err -a "docker(hsun9/ubuntu_xenial_py39)" "$@"
+bsub -G computer1 -q general -M ${MEM}000000 -n ${THREADS} -R "select[mem>${MEM}000] span[hosts=1] rusage[mem=${MEM}000]" -oo ${LOG}/${NAME}.log -eo ${LOG}/${NAME}.err -a "docker(hsun9/ubuntu_xenial_py39)" "$@"
 
 
